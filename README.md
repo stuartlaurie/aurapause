e4fcc9ab8012b1319279febfd57dd1e9077300ee
Stuart Laurie <stulaurie@gmail.com>
2024-04-23 15:28:42 -0700

    initial commit of pause utility

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
 # aurapause
-Scripts using Neo4j Aura API to list instances and pause them
+Script using Neo4j Aura API to list instances and pause them
+
+## Usage
+
+Generate Neo4j Aura credentials following these [instructions in Neo4j documentation](https://neo4j.com/docs/aura/platform/api/authentication/#_creating_credentials)
+
+
+### Generate Report
+
+If you run the script and provide credentials file saved from above steps you will get a list of databases and their status
+
+```
+python3 aurapause.py -c auracreds.txt
+```
+
+This will generate output similar to the following
+
+```
+  tenant_id   name        id   status memory storage cloud_provider        region
+1   tenant1  test0  54b2eda9  running   24GB    48GB            gcp   us-central1
+2   tenant1  test1  7af24124  running    8GB    16GB            gcp   us-central1
+4   tenant1  test2  e27e5959  running    8GB    16GB            gcp   us-central1
+0   tenant1  test3  81381681   paused    NaN     NaN            gcp   us-central1
+3   tenant1  test4  882231c2   paused    NaN     NaN            gcp   us-central1
+5   tenant2  test5  03666a67   paused    NaN     NaN            gcp  europe-west1
+```
+
+You can also specify specific tenant id to limit to only databases in that tenant
+
+```
+python3 aurapause.py -c auracreds.txt -t tenant2
+```
+
+will filter to specified tenant
+
+
+```
+  tenant_id   name        id  status memory storage cloud_provider        region
+0   tenant2  test5  03666a67  paused    NaN     NaN            gcp  europe-west1
+```
+
+### Pausing databases
+
+There are 3 options to pause databases, either specify ALL or a list of database ids
+
+#### Pausing specific running databases
+
+```
+python3 aurapause.py -c auracreds.txt -p database1,database2
+```
+
+#### Pausing all running databases
+
+Attempt to pause all running instances:
+
+```
+python3 aurapause.py -c auracreds.txt -p ALL
+```
+
+This can also be filtered by tenant 
+
+```
+python3 aurapause.py -c auracreds.txt -t tenant2 -p ALL
+```
+
+If you want to exclude certain databases you can use the additional -e flag with a list of databases
+
+```
+python3 aurapause.py -c auracreds.txt -p ALL -e database1,database2
+```
+
---
+++ aurapause.py
@@ -0,0 +1,132 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import pandas as pd
+from argparse import ArgumentParser
+from tqdm import tqdm
+
+auraendpoint = "https://api.neo4j.io/v1/instances"
+
+########################################
+##  SETUP command line arguments
+########################################
+
+parser = ArgumentParser(prog='aurapause.py',
+                    description='Lists and pauses Neo4j Aura instances',)
+parser.add_argument("-c", "--credentials", dest="credfile",
+                    help="file containing aura API credentials")
+parser.add_argument("-p", "--pause", dest="pause", default="",
+                    help="whether to pause instance, ALL or comma separated list of ids")
+parser.add_argument("-e", "--exclude", dest="exclude", default="", 
+                    help="instances to exclude from pause, comma separated list of ids")
+parser.add_argument("-t", "--tenant", dest="tenant", default="", 
+                    help="tenant id, filter only to dbs in specified tenant")
+
+args = parser.parse_args()
+
+########################################
+##  READ CREDS AND AUTHENTICATE
+########################################
+
+## Read aura credentials
+config = {}
+with open(args.credfile, "r") as configs:
+    for line in configs:
+        line = line.strip()
+        name, var = line.partition("=")[::2]
+        config[name.strip()] = str(var)
+
+## Make the request to the Aura API Auth endpoint
+print ("Authorizing.. ")
+response = requests.request(
+    "POST",
+    "https://api.neo4j.io/oauth/token",
+    headers={"Content-Type": "application/x-www-form-urlencoded"},
+    data={"grant_type": "client_credentials"},
+    auth=HTTPBasicAuth(config['CLIENT_ID'], config['CLIENT_SECRET'])
+)
+
+API_Data = response.json()
+# print (API_Data['access_token'])
+## set auth header
+headers = {"Authorization": "Bearer "+API_Data['access_token'], "Content-Type": "application/json"}
+
+########################################
+##  GET INSTANCE LIST
+########################################
+
+print ("Getting list of instances.. ")
+instances = requests.get(auraendpoint,headers=headers).json()
+instancelistdf=pd.DataFrame(instances['data'])
+if args.tenant != "":
+    if args.tenant in instancelistdf['tenant_id'].values:
+        instancelistdf = instancelistdf.loc[instancelistdf['tenant_id'] == args.tenant]
+    else:
+        ## TODO: could also use the tenant API endpoint to validate if exists
+        print("No databases found for tenant: " + args.tenant)
+        exit()
+
+print ("Found " + str(len(instancelistdf)) + " databases")
+
+########################################
+##  GET DB DETAILS/STATUS
+########################################
+
+print ("Getting instance details.. ")
+instancedetails=[]
+
+for dbid in tqdm(instancelistdf['id']):
+    instanceendpoint = auraendpoint+"/"+dbid
+    instancedetail = requests.get(instanceendpoint,headers=headers).json()
+    instancedetails.append(instancedetail['data'])
+
+########################################
+##  SHOW DBs
+########################################
+
+print ("Running Instances:")
+instancedf = pd.DataFrame(instancedetails)
+## get rid of ..GB and make int for correct sort
+## when paused we don't get memory/storage info so add empty columns
+if 'memory' in instancedf:
+    instancedf["memoryint"] = instancedf.memory.str[:-2]
+    instancedf["memoryint"] = instancedf["memoryint"].replace('NaN', pd.NA).fillna(0).astype(int)
+    instancedf.sort_values(by=['status', 'memoryint'],ascending=[False,False],inplace=True)
+else:
+    instancedf["memory"]='NaN'
+if 'storage' not in instancedf:
+    instancedf["storage"]='NaN'
+
+print (instancedf[['tenant_id','name','id','status','memory','storage','cloud_provider','region']])
+
+
+########################################
+##  PAUSING
+########################################
+
+def pause_db(pause_list):
+    for dbid in pause_list:
+        print ("Attempting to pause db with ID(s): " + dbid)
+        pauseendpoint = auraendpoint+"/"+dbid+"/pause"
+        pausedetail = requests.post(pauseendpoint,headers=headers).json()
+        print(pausedetail)
+
+## Check to see if we want to pause any
+pause_list=[]
+if args.pause != "":
+    if args.pause == "ALL":
+        print ("Pausing all running DBs..")
+        pause_list=instancedf.loc[instancedf['status'] == 'running', 'id'].tolist()
+        if args.exclude != "":
+            exclude_list = args.exclude.split(",")
+            print ("Excluding DB ids: " + ','.join(exclude_list))
+            for exclude_db in exclude_list:
+                if exclude_db in pause_list:
+                    pause_list.remove(exclude_db)
+                else:
+                    print ("Warning: " + exclude_db + " Not found in list of databases")
+    else:
+        print ("Pausing: "+args.pause)
+        pause_list = args.pause.split(",")
+    print(pause_list)
+    pause_db(pause_list)
+
